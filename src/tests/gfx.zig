9e3b2ffa3b2f5d04cc3d4877f7705b5f43725ebe
InSearchOfName <128755553+InSearchOfName@users.noreply.github.com>
2026-09-01 09:20:31 +0200

    extra testcases for the gfx module

--- src/tests/gfx.zig
+++ src/tests/gfx.zig
@@ -9,3 +9,17 @@ test "gfx should draw a pixel in the top left corner" {
     gfx.gfx_draw_pixel(0, 0);
     std.debug.assert(gfx.gfx_get_pixel(0, 0) == 1);
 }
+
+test "gfx should be cleared on clear" {
+    gfx.gfx_init();
+    gfx.gfx_draw_pixel(0, 0);
+    gfx.gfx_clear();
+    std.debug.assert(gfx.gfx_get_pixel(0, 0) == 0);
+}
+
+test "gfx should ignore pixels drawn outside of the drawing grid" {
+   gfx.gfx_init();
+   gfx.gfx_draw_pixel(800,800);
+   
+}
+
